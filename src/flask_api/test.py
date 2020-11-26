25e951533aca0b104cb64eb504b58e0b25e8d6e7
Daniel Klauber <daniel.klauber@factoryshepherd.de>
2020-11-26 21:12:11 +0100

    Added requirements.txt and included finanzen.net API

--- src/flask_api/test.py
+++ src/flask_api/test.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import finanzen_fundamentals.stocks as ff
 
 app = Flask(__name__)
 
 
 @app.route("/<ticker>")
 def print_ticker(ticker):
-    return "Ticker: %s!" % ticker
+    ff.search_stock("519000")
+    return 1
 
 
 if __name__ == "__main__":
